d46cb90512278a4dab32b872dd03e771a2fd5951
Om Gupta <ogupta4242@gmail.com>
2025-09-28 14:46:07 -0700

    feat(mongu): provide access to configuration

--- src/main/kotlin/xyz/malefic/frc/pingu/Mongu.kt
+++ src/main/kotlin/xyz/malefic/frc/pingu/Mongu.kt
@@ -16,13 +16,25 @@ class Mongu<T : Any>(
     val motor: T,
 ) {
     /**
+     * Holds the last configuration applied to this motor.
+     * This property is updated whenever the `configure` method is called.
+     * It allows retrieval of the configuration settings for inspection or reuse.
+     */
+    lateinit var configuration: MotorConfig<T>
+
+    init {
+        require(motor is TalonFX || motor is PWMTalonSRX) { "Unsupported motor type" }
+        configure()
+    }
+
+    /**
      * Configures the motor using a DSL-style configuration block.
      *
      * @param block A lambda that applies configuration settings to the motor.
      * @throws IllegalArgumentException If the motor type is unsupported.
      */
     @Suppress("UNCHECKED_CAST")
-    fun configure(block: MotorConfig<out T>.() -> Unit) {
+    fun configure(block: MotorConfig<out T>.() -> Unit = {}) {
         val config =
             when (motor) {
                 is TalonFX -> TalonFXConfig().apply(block as MotorConfig<TalonFX>.() -> Unit)
@@ -30,6 +42,7 @@ class Mongu<T : Any>(
                 else -> throw IllegalArgumentException("Unsupported motor type")
             } as MotorConfig<T>
         config.applyTo(motor)
+        configuration = config
     }
 }
 
